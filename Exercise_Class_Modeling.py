6f4332df9439b263a54ec6b79a01be2d31c7909d
ChrisDev10github <102984870+ChrisDev10github@users.noreply.github.com>
2022-04-11 22:05:21 -0500

    Add files via upload

---
+++ Exercise_Class_Modeling.py
@@ -0,0 +1,65 @@
+#Exeercise Class Modeling
+
+class Animal:
+
+    """Animal"""
+
+    def __init__(self, name):
+        self.__name = name
+
+    @property
+    def name(self):
+        print("Getting Name")
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        print("Setting Name")
+        self.__name = new_name
+
+    def move(self):
+        print('Moving')
+
+
+
+
+class Book:
+
+    """Book"""
+
+    def __init__(self, author):
+        self.__author = author
+
+    @property
+    def author(self):
+        print("Getting Author")
+        return self.__author
+
+    @author.setter
+    def name(self, new_author):
+        print("Setting author")
+        self.__name = new_author
+
+    def collectingdust(self):
+        print('Collecting Dust')
+
+
+class Vehicle:
+
+    """Vehicle"""
+
+    def __init__(self, model):
+        self.__model = model
+
+    @property
+    def model(self):
+        print("Getting Model")
+        return self.__author
+
+    @Model.setter
+    def name(self, new_Model):
+        print("Setting Model")
+        self.__name = new_Model
+
+    def accelerate_when_yellow(self):
+        print('SpeedingUp')
